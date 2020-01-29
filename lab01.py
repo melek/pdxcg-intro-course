bd8ca77228320baa824ce9a0a910ed9c55f34a0c
Lionel Di Giacomo <lionel@buttonpoetry.com>
2020-01-28 23:19:38 -0800

    Clean up and and comment on first lab

--- lab01.py
+++ lab01.py
@@ -1,7 +1,14 @@
+'''
+Lab 1: Hello
+Assignment by Lionel Di Giacomo
+1/21/2020
+
+This introductory 'lab' was just testing essential language constructs.
+'''
 # This is a single line comment in python. It is useful for ignoring a line of code quickly.
 
 # Prints to the terminal the provided string on a new line. 
-# print("Hello World!")
+print("Hello World!")
 
 '''
     This is a long python comment.
@@ -10,8 +17,12 @@
 
 """ Double quotes create comments as well. """
 
+# Variable declaration
 greeting = "I hope you are all having a great day!"
+
+# Simple for loops
 for x in range(5):
+    # Casting a string to int for concatenation
     print(str(x) + ": " + greeting)
 
 Mouse = "mickey"
